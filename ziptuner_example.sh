8119756b3fa65a2a9b00acaa29a9e6b806e446e9
deeice <dheyse@hotmail.com>
2017-03-19 08:32:22 -0400

    Updated example ziptuner startup script.

--- ziptuner_example.sh
+++ ziptuner_example.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+# With a dark and edgy theme for the dialogs:
+# Go to the ziptuner directory and run ziptuner,
+# using mpg123 on an open vt to play mp3 streams,
+# or mplayer on an open vt to play anything else.
+# Use killall to stop any of the players.
+# Save individual URL files to the ~/radio directory.
+# Also append URLS to the gmu playlist file.
+
 cd /usr/share/ziptuner
-TERM=xterm-color DIALOGRC=/usr/share/ziptuner/dialogrc.soho ziptuner -p "mplayertty4 -playlist " -s "killall mpg123; killall mplayer" /root/radio /root/.local/share/gmu/playlist.m3u
+
+DIALOGRC=/usr/share/ziptuner/dialogrc.soho ziptuner -mp3 "mpg123tty4 -@ " -p "mplayertty4 -playlist " -s "killall mpg123; killall mplayer" ~/radio ~/.config/gmu/playlist.m3u
