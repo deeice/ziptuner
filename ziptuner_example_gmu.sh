d0b0f3bd99b0027bf791bdd1a8490b6fe9c79b92
deeice <dheyse@hotmail.com>
2020-03-14 17:09:46 -0400

    Forgot gmusrv was deprecated.  Darn it.

--- ziptuner_example_gmu.sh
+++ ziptuner_example_gmu.sh
@@ -2,4 +2,7 @@
 # Use headless gmu server to play, and a multistream playlist for storage.
 # The stop cmd could be improved to stop playing, but leave the server running.
 
-./ziptuner -p "SDL_AUDIODRIVER=alsa LD_LIBRARY_PATH=libs.z2/ ./gmu -c gmu.zipit-z2.conf -p frontends.old/gmusrv.so $@ >/dev/null 2>&1 " -s "gmu-cli x" playlist.m3u
+#./ziptuner -p "SDL_AUDIODRIVER=alsa LD_LIBRARY_PATH=libs.z2/ ./gmu -c gmu.zipit-z2.conf -p frontends.old/gmusrv.so $@ >/dev/null 2>&1 " -s "gmu-cli x" playlist.m3u
+
+# gmusrv, gmu-cli are deprecated in gmu 10 so use fake frontend (or try gmuhttp)
+./ziptuner -p "SDL_AUDIODRIVER=alsa LD_LIBRARY_PATH=libs.z2/ ./gmu -c gmu.zipit-z2.conf -p dummy $@ >/dev/null 2>&1 " -s "killall -9 gmu" playlist.m3u
