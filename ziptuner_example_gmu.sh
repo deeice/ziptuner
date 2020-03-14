43a666eb0f6707eec980e4d231de10a4ada6f115
deeice <dheyse@hotmail.com>
2020-03-14 15:13:57 -0400

    updated gmu example script to use gmu-cli for stop

--- ziptuner_example_gmu.sh
+++ ziptuner_example_gmu.sh
@@ -2,4 +2,4 @@
 # Use headless gmu server to play, and a multistream playlist for storage.
 # The stop cmd could be improved to stop playing, but leave the server running.
 
-./ziptuner -p "SDL_AUDIODRIVER=alsa LD_LIBRARY_PATH=libs.z2/ ./gmu -c gmu.zipit-z2.conf -p frontends.old/gmusrv.so $@ >/dev/null 2>&1 " -s "killall -9 gmu" playlist.m3u
+./ziptuner -p "SDL_AUDIODRIVER=alsa LD_LIBRARY_PATH=libs.z2/ ./gmu -c gmu.zipit-z2.conf -p frontends.old/gmusrv.so $@ >/dev/null 2>&1 " -s "gmu-cli x" playlist.m3u
