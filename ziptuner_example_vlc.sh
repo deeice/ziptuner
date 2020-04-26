ce398381a6af9462df9deca021408fcd464d9223
deeice <dheyse@hotmail.com>
2020-04-26 12:30:16 -0400

    Add auto-resume dialog to vlc example script

--- ziptuner_example_vlc.sh
+++ ziptuner_example_vlc.sh
@@ -1,3 +1,10 @@
 
+# First, put up a dialog to ask about auto-resume.  Then launch ziptuner.
+if [ -f ziptuner.fav ] ; then
+  dialog --clear --title "Zippy Internet Radio Tuner" --yesno "Resume Autoplay?" 0 0
+  [ $? -eq 0 ] && A="-a" || A=
+fi
+
+
 # Use headless vlc (dummy interface) and a multistream playlist for storage. 
-./ziptuner -p "vlc -I dummy -q >/dev/null 2>&1 " -s "killall vlc" playlist.pls
+./ziptuner $A -p "vlc -I dummy -q >/dev/null 2>&1 " -s "killall vlc" playlist.pls
